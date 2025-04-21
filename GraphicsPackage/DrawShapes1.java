407efbeefa220c25486c1dfd67e8d594097e3730
ShefeenaHaris <shefeenaharis02@gmail.com>
2025-04-21 10:09:00 +0530

    Add files via upload

---
+++ GraphicsPackage/DrawShapes1.java
@@ -0,0 +1,14 @@
+import java.applet.Applet;
+import java.awt;
+public class DrawShapes1 extends Applet
+{
+public void paint(Graphics g)
+{
+g.setColor(Color,Green);
+g.drawLine(50,200,300,200);
+g.setColor(Color,RED);
+s.drawRect(50,50,150,100);
+g.setColor(Color,BLUE);
+g.drawOval(250,50,100,100);
+}
+}
